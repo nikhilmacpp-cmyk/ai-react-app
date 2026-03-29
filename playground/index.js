6bdfe1f0089e71cefed9dab676922af0738a94cb
Nikhil PP <nikhilmac.pp@gmail.com>
2026-03-29 15:00:47 +0530

    move api key to .env

--- playground/index.js
+++ playground/index.js
@@ -1,13 +1,26 @@
-
-const getAIPromptResponse = async (promptFromUser = '' ) => {
+import dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
+import { GoogleGenAI } from "@google/genai";
+const ai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY
+});
+console.log("KEY:", process.env.GEMINI_API_KEY);
+const getAIPromptResponse = async (promptFromUser = '') => {
     try {
-        
-        
-        const data = await response.json();
-        return data
+        let finalText = ""
+        const stream = await ai.models.generateContentStream({
+            model: "gemini-3-flash-preview",
+            contents: promptFromUser,
+        })
+        for await (const chunk of stream){
+            if(chunk.text){
+                finalText+= chunk.text}
+        }
+        return finalText;
     } catch (er) {
         console.log('Prompt error', er);
     }
 }
-const promptText = 'joke for the day' 
-const getRes = await getAIPromptResponse(promptText)
+const promptText = 'write a paragraph'
+const getRes = await getAIPromptResponse(promptText);
+console.log("AI Output:", getRes);
