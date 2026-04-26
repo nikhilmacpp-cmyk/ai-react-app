7b3299a2701af707f8f9246829380e77bd185c3d
Nikhil PP <nikhilmac.pp@gmail.com>
2026-04-26 12:22:13 +0530

    Rag LLM implementation

--- customChat/backend/rag.py
+++ customChat/backend/rag.py
@@ -6,5 +6,26 @@ from langchain_community.chat_models import ChatOllama
 documents = [
     "Company allows 10 days leave per year",
     "Office timing is 9 AM to 5 PM",
-    "Refund policy is 7 days"
-]
+    "Refund policy is 7 days",
+    "JavaScript is a programming language used for web development"
+];
+
+embeddings = HuggingFaceEmbeddings()
+db = FAISS.from_texts(documents,embeddings);
+llm = ChatOllama(model = "llama3")
+
+query = input("Ask Something :: ")
+results = db.similarity_search_with_score(query, k=1)
+
+docs, score = results[0]
+
+print(score)  # debug
+
+if score > 1.0:   # tune this threshold
+    print("\query",query)
+    prompt = query
+else:
+    prompt = docs.page_content
+    
+response = llm.invoke(prompt)
+print("\nresponse",response)
